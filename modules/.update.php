fdeb935650ff2d5e0ee15ceb784d483cd7de4750
Aoxar <63445750+Aoxar@users.noreply.github.com>
2020-08-23 10:13:28 +0530

    Update .update.php

--- modules/.update.php
+++ modules/.update.php
@@ -7,32 +7,27 @@ function logo() {
   system("clear");
   echo <<<EOL
 \033[01;33m
-
-
 \033[01;31m      _\033[01;33m ____    _
      \033[01;31m(_)\033[01;33m  _ \  | |_ _ __ __ _  ___ ___ _ __
      | | |_) | | __| '__/ _` |/ __/ _ \ '__|
      | |  __/  | |_| | | (_| | (_|  __/ |
      |_|_|      \__|_|  \__,_|\___\___|_|
-
-
    \033[01;37m}\033[01;31m----------------------------------------\033[01;37m{
 }\033[01;31m-------------- \033[01;32mTrack IPLocation\033[01;31m --------------\033[01;37m{
    }\033[01;31m----------------------------------------\033[01;37m{
-
 \033[00m
 EOL;
 }
 
 function upd() {
   logo();
-  echo "\n\033[01;32mUpdating IP-Tracer.........\033[01;37m\n\n";
+  echo "\n\033[01;32mUpdating Dox-ip.........\033[01;37m\n\n";
   sleep(1);
-  system("cd ~/ && git clone https://github.com/rajkumardusad/IP-Tracer.git");
-  system("cd ~/ && sudo git clone https://github.com/rajkumardusad/IP-Tracer.git");
-  system("cd ~/IP-Tracer && sh install");
+  system("cd ~/ && git clone https://github.com/Aoxar/Dox-ip.git");
+  system("cd ~/ && sudo git clone https://github.com/Aoxar/Dox-ip.git");
+  system("cd ~/Dox-ip && sh install");
   logo();
-  echo "\n\033[01;32m              IP-Tracer updated !!!\033[01;37m\n";
+  echo "\n\033[01;32m              Dox-ip updated !!!\033[01;37m\n";
   sleep(1);
 }
 upd();
